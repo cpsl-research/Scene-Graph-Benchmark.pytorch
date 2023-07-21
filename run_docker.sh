cca4f7a25708bd5e66e281283855351e83e4db78
spencer@primus <spencer.hallyburton@duke.edu>
2023-07-21 15:24:49 -0400

    Remove folder check from docker

--- run_docker.sh
+++ run_docker.sh
@@ -2,44 +2,19 @@
 
 set -e 
 
-DATAFOLDER=${1:-/data/$(whoami)/}
-MODELFOLDER=${2:-/data/$(whoami)/models}
-
-DATAFOLDER=${DATAFOLDER%/}  # remove trailing slash
-MODELFOLDER=${MODELFOLDER%/}  # remove trailing slash
-
 xhost local:root
 
 # pull docker image
 docker pull roshambo919/scenegraph:benchmark
 
-# check if the data folders are populated before continuing
-if [ -z "$(ls -A $DATAFOLDER)" ]; then
-   echo "\nERROR: nothing found in the data directory at $DATAFOLDER. Ensure you have downloaded the data before starting container"
-   exit
-else
-   echo "Data directory not empty...assuming it is populated with data!"
-fi
-if [ -z "$(ls -A $MODELFOLDER)" ]; then
-   echo "\nERROR: nothing found in the model directory at $MODELFOLDER. Ensure you have downloaded models before starting container"
-   exit
-else
-   echo "Model directory not empty...assuming it is populated with models!"
-fi
-
-
 # function to start docker
 start_docker () {
-    DF=$1
-    MF=$2
     echo "Starting fresh docker container"
     docker run \
       --name sgg-benchmark \
       --privileged \
       --runtime=nvidia \
       --gpus 'all,"capabilities=graphics,utility,display,video,compute"' \
-      --mount type=bind,src="$DATAFOLDER",target=/data \
-      --mount type=bind,src="$MODELFOLDER",target=/models \
       -p 8080:8080 \
       -it \
       roshambo919/scenegraph:benchmark \
@@ -60,7 +35,7 @@ fi
 
 # Start up a docker container
 echo "Starting up docker container"
-start_docker $DATAFOLDER $MODELFOLDER
+start_docker
 
 
 exit 0
